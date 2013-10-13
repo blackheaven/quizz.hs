a40bad4a0517ea25af6e23176e3f9bd91608796e
Gautier DI FOLCO <gautier.difolco@gmail.com>
2013-10-13 18:04:10 +0200

    Adding the core module

--- src/Quizz/Core.hs
+++ src/Quizz/Core.hs
@@ -1,4 +1,40 @@
-module Quizz.Core (add)  where
+module Quizz.Core (
+        Proposition(Proposition), isValid, content,
+        Question(Question), title, propositions,
+        Answers(Answers), answers,
+        Corrections(Corrections), corrections,
+        Correction(Correct, Wrong, Missed, NotCheckedNotAnswer), correct
+        )  where
 
-add :: Integer -> Integer -> Integer
-add = (+)
+data Proposition = Proposition  { content :: String
+                                , isValid :: Bool
+                                } deriving (Show)
+
+data Question = Question    { title :: String
+                            , propositions :: [Proposition]
+                            } deriving (Show)
+
+newtype Answers = Answers { answers :: [Integer] } deriving (Show)
+
+newtype Corrections = Corrections { corrections :: [Correction] } deriving (Show, Eq)
+
+data Correction = Correct | Wrong | Missed | NotCheckedNotAnswer deriving (Show, Eq)
+
+correct :: Question -> Answers -> Corrections
+correct (Question _ propositions) (Answers answersIndices) =
+    Corrections $ zipWith check propositions answersChecked
+    where answersChecked = switchTrue (map (subtract 1) answersIndices) $ replicate (length propositions) False
+
+check :: Proposition -> Bool -> Correction
+check (Proposition _ True) True = Correct
+check (Proposition _ True) False = Missed
+check (Proposition _ False) True = Wrong
+check (Proposition _ False) False = NotCheckedNotAnswer
+
+switchTrue :: [Integer] -> [Bool] -> [Bool]
+switchTrue indices set = switchTrue' 0 indices set []
+    where   switchTrue' _ [] s acc = (reverse acc) ++ s
+            switchTrue' _ _ [] acc = reverse acc
+            switchTrue' indice (i:is) (x:xs) acc
+                | i == indice = switchTrue' (indice + 1) is xs (True:acc)
+                | otherwise = switchTrue' (indice + 1) (i:is) xs (x:acc)
--- test/Quizz/CoreSpec.hs
+++ test/Quizz/CoreSpec.hs
@@ -8,8 +8,55 @@ main = hspec spec
 
 spec :: Spec
 spec = do
-  describe "add" $ do
-    it "21 + 21 = 42" $ do
-      add 21 21 `shouldBe` 42
-    it "20 + 20 = 20" $ do
-      add 20 20 `shouldBe` 20
+  describe "Proposition datatype" $ do
+    describe "isValid getter" $ do
+      it "A valid proposition" $ do
+        isValid (Proposition "A" True) `shouldBe` True
+      it "An invalid proposition" $ do
+        isValid (Proposition "A" False) `shouldBe` False
+
+    describe "content getter" $ do
+      it "A simple content" $ do
+        content (Proposition "A" True) `shouldBe` "A"
+
+  describe "Question datatype" $ do
+    describe "title getter" $ do
+      it "A simple title" $ do
+        title (Question "A" []) `shouldBe` "A"
+
+    describe "propositions getter" $ do
+      it "No proposition" $ do
+        (length . propositions) (Question "A" []) `shouldBe` 0
+      it "One proposition" $ do
+        (length . propositions) (Question "A" [Proposition "A" True]) `shouldBe` 1
+      it "Two propositions" $ do
+        (length . propositions) (Question "A" [Proposition "A" True, Proposition "A" True]) `shouldBe` 2
+
+  describe "Answers datatype" $ do
+    describe "answers" $ do
+      it "No answer" $ do
+        (length . answers) (Answers []) `shouldBe` 0
+      it "One answer" $ do
+        (length . answers) (Answers [1]) `shouldBe` 1
+      it "Two answers" $ do
+        (length . answers) (Answers [1, 4]) `shouldBe` 2
+
+  describe "Corrections datatype" $ do
+    describe "corrections" $ do
+      it "No correction" $ do
+        (length . corrections) (Corrections []) `shouldBe` 0
+      it "One correction" $ do
+        (length . corrections) (Corrections [Correct]) `shouldBe` 1
+      it "Two corrections" $ do
+        (length . corrections) (Corrections [Wrong, Missed]) `shouldBe` 2
+    describe "correct" $ do
+      it "One proposition, one good answer, correctly answered" $ do
+        correct (Question "A" [Proposition "A" True]) (Answers [1])`shouldBe` (Corrections [Correct])
+      it "One proposition, one good answer, not answered" $ do
+        correct (Question "A" [Proposition "A" True]) (Answers [])`shouldBe` (Corrections [Missed])
+      it "One proposition, no good answer, badly answered" $ do
+        correct (Question "A" [Proposition "A" False]) (Answers [1])`shouldBe` (Corrections [Wrong])
+      it "One proposition, no good answer, correctly answered" $ do
+        correct (Question "A" [Proposition "A" False]) (Answers [])`shouldBe` (Corrections [NotCheckedNotAnswer])
+      it "Four propositions, two good answers, one correctly answered, one badly answered, one forgot" $ do
+        correct (Question "A" [Proposition "A" True, Proposition "A" True, Proposition "A" False, Proposition "A" False]) (Answers [1, 3])`shouldBe` (Corrections [Correct, Missed, Wrong, NotCheckedNotAnswer])
